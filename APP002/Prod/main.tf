9daeda7d10379ba1ea548a8534b8d0b5d80458b2
alicloudreach <sardar.ali@cloudreach.com>
2022-10-03 14:58:23 -0700

    initialCommit

--- APP002/Prod/main.tf
+++ APP002/Prod/main.tf
@@ -6,6 +6,18 @@ data "aws_vpc" "mainVPC" {
 
   
 }
+
+data "aws_subnet" "subnet_app_A" {
+  filter {
+    name = "tag:Name"
+    values = ["snet-prod-use1-application-a"]
+  }
+}
+
+data "aws_kms_key" "customerKey" {
+  key_id = "713104d4-357c-4ced-8b21-741029ab1173"
+}
+
 output "vpc_id" {
     value = data.aws_vpc.mainVPC.id
   
--- APP002/Prod/securitygroups.tf
+++ APP002/Prod/securitygroups.tf
@@ -20,3 +20,43 @@ resource "aws_security_group" "app_security_group" {
 
   
 }
+
+resource "aws_security_group" "db_security_group" {
+  name        = "app002-prod-use1-db"
+  description = "Database security for APP002"
+  vpc_id      = data.aws_vpc.mainVPC.id
+
+  ingress {
+    description      = "Ingress traffic from ALB"
+    from_port        = 1433
+    to_port          = 1433
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  tags = {
+    Department = "IT",
+    Name = "sg-app002-prod-use1-db",
+    AppID = "APP002",
+    Env = "Prod"
+  }  
+}
+
+resource "aws_security_group" "lb_security_group" {
+  name        = "app002-prod-use1-alb"
+  description = "LoadBalancer security for APP002"
+  vpc_id      = data.aws_vpc.mainVPC.id
+
+  ingress {
+    description      = "Ingress traffic from ALB"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  tags = {
+    Department = "IT",
+    Name = "app002-prod-use1-alb",
+    AppID = "APP002",
+    Env = "Prod"
+  }  
+}
