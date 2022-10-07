13c91cfcc7142f71d829fcd6e560633b46e56a18
2181 <zbigniew.balcerzyk@cloudreach.com>
2022-10-07 17:59:20 +0100

    upd

--- AP003/PROD/alb.tf
+++ AP003/PROD/alb.tf
@@ -6,7 +6,8 @@ resource "aws_lb" "app003-alb-prod" {
   subnets            = [data.aws_subnet.public_subnet_a.id, data.aws_subnet.public_subnet_b.id]
 
   tags = {
-    Environment = "production"
+    Environment = "production",
+    Alb = "Public"
   }
 }
 
@@ -27,7 +28,7 @@ resource "aws_lb_listener" "app003_port_80" {
   load_balancer_arn = aws_lb.app003-alb-prod.arn
   port              = "80"
   protocol          = "HTTP"
- 
+
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.app003_target_group.arn
--- AP003/PROD/instances.tf
+++ AP003/PROD/instances.tf
@@ -1,9 +1,10 @@
 resource "aws_instance" "app003-instance1" {
   ami           = "ami-026b57f3c383c2eec"
-  instance_type = "t2.medium"
+  instance_type = "t2.large"
+
 
   root_block_device {
-    volume_size = 20
+    volume_size = 50
     volume_type = "gp3"
     encrypted   = true
     kms_key_id  = data.aws_kms_key.customer_key.arn
@@ -18,9 +19,25 @@ resource "aws_instance" "app003-instance1" {
   })
 }
 
+resource "aws_ebs_volume" "extra_volume_app" {
+  availability_zone = data.aws_subnet.application_subnet.availability_zone
+  size              = 10
+
+  tags = {
+    Name = "extra-volume-app"
+  }
+}
+
+resource "aws_volume_attachment" "ebs_attachment_app" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.extra_volume_app.id
+  instance_id = aws_instance.app003-instance1.id
+}
+
 resource "aws_instance" "APP003-WDP01" {
   ami           = "ami-026b57f3c383c2eec"
   instance_type = "t2.large"
+
 
   root_block_device {
     volume_size = 30
@@ -33,7 +50,10 @@ resource "aws_instance" "APP003-WDP01" {
     network_interface_id = aws_network_interface.app_database_eni.id
     device_index         = 0
   }
-  tags = merge(local.tags, {
-    Name = "APP003-WDP01"
-  })
+  tags = {
+    Name = "APP003-WDP01",
+    DBA = "Support",
+    Department = "IT",
+    APPID = "APP003"
+  }
 }
