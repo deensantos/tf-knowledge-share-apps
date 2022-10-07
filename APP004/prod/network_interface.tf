c75132c2e62d2bd8335d399827f04045402d998b
MaanKaur Namdhari <maankaur.namdhari@cloudreach.com>
2022-10-07 11:36:03 -0700

    Terraform training

--- APP004/prod/network_interface.tf
+++ APP004/prod/network_interface.tf
@@ -9,3 +9,9 @@ resource "aws_network_interface" "instance_nic_app_b" {
   private_ips     = ["10.0.2.100"]
   security_groups = [aws_security_group.app_sg.id]
 }
+
+resource "aws_network_interface" "instance_nic_db" {
+  subnet_id       = data.aws_subnet.db_subnet.id
+  private_ips     = ["10.0.3.100"]
+  security_groups = [aws_security_group.database_sg.id]
+}
