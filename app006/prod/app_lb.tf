3b9fbcf0ef0f389bd3fdcd18158113544c58a578
Grant Balkin <grant.balkin@cloudreach.com>
2022-10-12 17:34:26 +0100

    update tf code

--- app006/prod/app_lb.tf
+++ app006/prod/app_lb.tf
@@ -8,3 +8,31 @@ resource "aws_lb" "app006-alb-prod" {
 tags = merge(local.common_tags,{Name = "app006-alb-prod"})
 
 }
+
+resource "aws_lb_listener" "app06-alb-front_end" {
+  load_balancer_arn = aws_lb.app006-alb-prod.arn
+  port              = "80"
+  protocol          = "HTTP"
+  
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app06-alb-target-group.arn
+  }
+}
+resource "aws_lb_target_group" "app06-alb-target-group" {
+  name     = "app06-alb-target-group"
+  port     = 443
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default_vpc.id
+}
+resource "aws_lb_target_group_attachment" "app01_target_group" {
+  target_group_arn = aws_lb_target_group.app06-alb-target-group.arn
+  target_id        = aws_instance.app_instance_01.id 
+  port             = 443
+}
+resource "aws_lb_target_group_attachment" "app02_target_group" {
+  target_group_arn = aws_lb_target_group.app06-alb-target-group.arn
+  target_id        = aws_instance.app_instance_02.id
+  port             = 443
+}
